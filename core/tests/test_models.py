00733eb319b73cb9727238aa8f10b7773ee08e97
Tung Leu <leudeptrai@gmail.com>
2020-12-10 03:18:43 -0500

    added test for user

---
+++ core/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        Test creating new user with email
+        """
+        email = 'tungleu18@lmao.com'
+        password = 'test1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
