5860760dd24fb7f0ea95210198f68465218fa940
Tung Leu <leudeptrai@gmail.com>
2020-12-11 03:23:26 -0500

    added customized admin

--- core/models.py
+++ core/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
 # Create your models here.
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('User must have an email address')
+
         user = self.model(email=email, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_super_user(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
